fce9968d0befe80583ca4761b59231607aa2574f
alifaizan786-op <faizan.ali@irunadigital.com>
2022-02-21 23:07:14 -0500

    added change quote

--- client/src/components/Quote.jsx
+++ client/src/components/Quote.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 
-import { Typography, Button } from "@mui/material";
+import { 
+  Typography, 
+  Button,
+  Modal,
+  Input,
+  Box,
+  FormControl,
+  TextField
+} from "@mui/material";
+
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "30vw",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 5,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: 'center',
+  minWidth: "50%",
+  borderRadius: "30px",
+};
+
+
 
 export default function Quote() {
+
+  const [edit, setEdit] = React.useState(false);
+
+  const setEditTrue = () => {
+    setEdit(true);
+  };
+
+  const setEditFalse = () => {
+    setEdit(false);
+  };
+
   return (
     <div
       style={{
@@ -33,6 +72,7 @@ export default function Quote() {
       <Button
         type="submit"
         fullWidth
+        onClick={setEditTrue}
         variant="contained"
         sx={{
           fontSize: "20px",
@@ -43,6 +83,45 @@ export default function Quote() {
         }}>
         Change Quote
       </Button>
+      <Modal open={edit} onClose={setEditFalse}>
+        <Box sx={style}>
+          <Typography
+            variant="h4"
+            sx={{
+              fontFamily: "Baskervville",
+              textAlign: "center",
+              marginY: "5px",
+            }}>
+            Change Quote Of The Day
+          </Typography>
+          <FormControl>
+            <TextField
+              id="outlined-multiline-flexible"
+              sx={{marginTop:'10px', minWidth:'90%'}}
+              label="Description"
+              multiline
+              minRows={4}
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            onClick={setEditTrue}
+            variant="contained"
+            sx={{
+              fontSize: "20px",
+              bgcolor: "primary.main",
+              color: "primary.light",
+              marginTop:'auto',
+              width: "15%",
+              alignItems:'center',
+              borderRadius: "10px",
+            }}>
+            Save
+          </Button>
+
+        </Box>
+      </Modal>
     </div>
   );
 }
