4f8142be3eb84f3e4e05f4fec54fe51b3eeeb9f0
alifaizan786-op <faizan.ali@irunadigital.com>
2022-12-26 20:22:31 -0500

    forgot to add process.env

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 /* Connecting the database to the server. */
 mongoose.connect(
-	MONGODB_URI || 'mongodb://localhost/malani_employee_portal',
+	process.env.MONGODB_URI || 'mongodb://localhost/malani_employee_portal',
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
