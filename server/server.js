81f8e4ded0e413770b6b702dd2ada806ee737bad
alifaizan786-op <faizan.ali@irunadigital.com>
2022-12-26 20:01:40 -0500

    added concurrency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const throng = require('throng');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -10,44 +11,55 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.use(express.static(path.join(__dirname, "./images")));
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './images')));
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
-})
+	res.sendFile(path.join(__dirname, '../client/'));
+});
 
-setInterval(()=>{
-  if (today.getDay() !== 1){
-    renewTasks();
-  }
+setInterval(() => {
+	if (today.getDay() !== 1) {
+		renewTasks();
+	}
 }, 3600000);
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
-  
-// Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+// setup load balancing and concurrency using throng
+throng({
+	workers: process.env.WEB_CONCURRENCY || 4,
+	lifetime: Infinity,
+	start: start,
+});
+
+function start() {
+	// Create a new instance of an Apollo server with the GraphQL schema
+	const startApolloServer = async (typeDefs, resolvers) => {
+		await server.start();
+		server.applyMiddleware({ app });
+
+		db.once('open', () => {
+			app.listen(PORT, () => {
+				console.log(`API server running on port ${PORT}!`);
+				console.log(
+					`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+				);
+			});
+		});
+	};
+
+	// Call the async function to start the server
+	startApolloServer(typeDefs, resolvers);
+}
