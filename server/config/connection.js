be490c215971c17f7444a949d607e7798410d4dd
alifaizan786-op <faizan.ali@irunadigital.com>
2023-01-08 02:45:47 -0500

    rm atlas string

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 /* Connecting the database to the server. */
 mongoose.connect(
-	'mongodb+srv://malani:Malani2002@cluster0.lmrwitb.mongodb.net/MalaniEmployeePortalLive?retryWrites=true&w=majority',
-	// process.env.MONGODB_URI || 'mongodb://localhost/malani_employee_portal',
+	process.env.MONGODB_URI || 'mongodb://localhost/malani_employee_portal',
 	{
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
